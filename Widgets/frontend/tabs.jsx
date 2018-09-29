9383b423eca5e85c88db2aa1ef49c10cbea3422e
wade <wade.coufal@gmail.com>
2018-09-28 17:04:03 -0700

    Finish Clock/Tabs

--- Widgets/frontend/tabs.jsx
+++ Widgets/frontend/tabs.jsx
@@ -3,10 +3,29 @@ import React from 'react';
 class Tabs extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {index: 0};
+  }
+
+  toggleTab(idx) {
+    this.setState({index: idx});
   }
 
   render () {
-    
+    return (
+      <ul className="tab-widget">
+        <h1>Tabs</h1>
+        <div className="tab-headers">
+          {this.props.info.map( (tab, idx) => {
+            return (
+              <div key={ idx }>
+                <h1 className="tab-header" onClick={() => this.toggleTab(idx)}>{ tab.title }</h1>
+              </div>
+            );
+          })}
+        </div>
+        <article className="tab-content">{this.props.info[this.state.index].content}</article>
+      </ul>
+    );
   }
 }
 
